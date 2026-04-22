dbfed2b3926d5c718a27e11043cf1e2f51e80d35
john <carrizojohnny@gmail.com>
2026-04-22 20:24:37 -0300

    arrays

---
+++ js/arrays.js
@@ -0,0 +1,5 @@
+let semana = ["Lunes", "martes","fede", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+
+semana.forEach(dia => {console.log (dia)})
+
+console.log (document.querySelectorAll('.texto'))
